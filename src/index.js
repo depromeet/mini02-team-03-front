c698435837b99260455fa51a9716689cb39c00e9
Unknown <kimts2002kr@naver.com>
2018-04-29 12:38:39 +0900

    ISSUE-03 Scss 설정
    * scss 기본 셋팅

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Root from './mobweb/Root';
 import registerServiceWorker from './registerServiceWorker';
+import './css/styles.css';
 
 ReactDOM.render(<Root />, document.getElementById('root'));
 registerServiceWorker();
