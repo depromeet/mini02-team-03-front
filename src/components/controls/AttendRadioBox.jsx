e6ffdc29e811a767610239d19527cf1a76a9f0e6
Unknown <kimts2002kr@naver.com>
2018-05-11 20:35:59 +0900

    ISSUE-12 출석/불참 체크를 위한 라디오 버튼 제작

---
+++ src/components/controls/AttendRadioBox.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import onImg from '../../resources/controls/btn-atd-on.svg';
+import offImg from '../../resources/controls/btn-atd-off.svg';
+
+const AttendRadioBox = props => (
+  <label
+    htmlFor="AttendRadioBox"
+    style={props.cusStyle}
+    className="attend-radio-box"
+  >
+    <input
+      type="radio"
+      id={props.group}
+      name={props.group}
+      className="test"
+    />
+    <div className="box">
+      <span className="text">{props.text}</span>
+      <img src={onImg} className="on-img" alt="Checked circle" />
+      <img src={offImg} className="off-img" alt="Uncheck circle" />
+    </div>
+  </label>
+);
+
+export default AttendRadioBox;
--- src/components/study/StudyCard.jsx
+++ src/components/study/StudyCard.jsx
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
+import AttendRadioBox from '../controls/AttendRadioBox';
 import cardBgImg from '../../resources/bg-card.svg';
 import homeIcon from '../../resources/icons/ic-home-on.svg';
 import studyImg from '../../resources/img-1.svg';
+
 
 class StudyCard extends Component {
   render() {
@@ -12,6 +14,10 @@ class StudyCard extends Component {
           <img src={studyImg} alt="study-img" className="study-img" />
           <span className="title">{this.props.title}</span>
           <p className="description">{this.props.description}</p>
+        </div>
+        <div className="check-block">
+          <AttendRadioBox text="참석" group="attendance" />
+          <AttendRadioBox text="불참" group="attendance" cusStyle={{ 'margin-left': '1.7rem' }} />
         </div>
         <img src={cardBgImg} alt="bg" className="card-bg-img" />
       </div>
